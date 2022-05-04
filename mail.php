f1cf64c2449ebcf7813a63518988978aefe3105f
Kalvin <47543827+Kallywoo@users.noreply.github.com>
2022-05-04 20:28:30 +0100

    rm fb:like & fix email

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
   foreach($_POST as $key => $value) {
     $message .= "$key => $value\n";
   }
-   
-  mail('mat@genuinebuilders.co.uk,elaine@genuinebuilders.co.uk','****JOB ENQUIRY****',$message);
+  $headers = "From: me@example.com";
+  mail('mat@genuinebuilders.co.uk,elaine@genuinebuilders.co.uk','****JOB ENQUIRY****',$message,$headers);
   header('Location: contact.html');
 ?>
